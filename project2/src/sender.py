3c6ea10e75c763a304a056430960d08f312b911d
Laurie Delinois <lounedelinois@gmail.com>
2021-03-30 18:02:03 -0400

    update sender and receiver

--- project2/src/sender.py
+++ project2/src/sender.py
@@ -7,29 +7,25 @@ from socket import *
 import sys
 import time
 
-#s = socket(AF_INET,SOCK_DGRAM)
-#sys.stdin.readline()
+
 host =sys.argv[1]
-#port = 9999
+
 port = int(sys.argv[2])
 
 s = socket(AF_INET,SOCK_DGRAM)
 buf =1024
 addr = (host,port)
-#inFile = sys.argv[1]
-
 
 #f=open("a.txt","rb")
 #f = open("a.txt", "rb")
 
-#data = f.read(buf).encode()
+
 data = sys.stdin.read(buf).encode()
 start_time = time.time()
 total_kb = 0
 timeSec = 0
 
-while (data):
-  #for line in sys.stdin:  
+while (data):  
     sys.stdin.read(buf)
     if(s.sendto(data,addr)):
       #for line in sys.stdin: 
@@ -38,14 +34,11 @@ while (data):
         current_time = time.time()
         elapsed_time = current_time - start_time
         total_kb = total_kb + 1
-        
-      #for line in sys.stdin:
-        #data = f.read(buf)
       
 s.close()
-print("File received, exiting.")
-
 
 elapsed_time = float("%0.3f" % (elapsed_time))
-bytes = total_kb * 1000
-print("Sent " + str(bytes) + " bytes in " + str(elapsed_time) + " seconds: " + str(total_kb) + " kB/s")
+bytes = total_kb * 1024
+kb_sec = total_kb/60
+kb_sec = float("%0.3f" % (kb_sec))
+print("Sent " + str(bytes) + " bytes in " + str(elapsed_time) + " seconds: " + str(kb_sec) + " kB/s")
