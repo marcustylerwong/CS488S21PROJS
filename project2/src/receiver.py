92f143ef5a14f65a9c62cb576805ef7ed4858ac6
Laurie Delinois <lounedelinois@gmail.com>
2021-04-04 23:31:01 -0400

    receiver update

--- project2/src/receiver.py
+++ project2/src/receiver.py
@@ -6,9 +6,18 @@
 from socket import *
 import sys
 import select
+import time
 
+
+  
+  
+  
+
+#------------------------------------running main program------------------------------
+#sys.argv for testing
 #host = sys.argv[1]
 host = "127.0.0.1"
+#port = int(sys.argv[1])
 port = int(sys.argv[1])
 
 #start socket and bind to sender
@@ -17,7 +26,9 @@ s.bind((host,port))
 
 addr = (host,port)
 buf=1024
-
+counter = 0
+seqnum = counter
+ack = "Acknowledgement"
 
 #f = open("received.txt",'wb')
 
@@ -26,13 +37,44 @@ data,addr = s.recvfrom(buf)
 
 try:
     while(data):
+       
        sys.stdout.write(data.decode())
-       #f.write(data)
-       s.settimeout(2)
+       #data,addr = s.recvfrom(buf)
+       counter = counter + 1
+       seqnum = counter
+    
+       s.settimeout(3)
+    
        data,addr = s.recvfrom(buf)
+       #sys.stderr.write(str(data))
+       #if(s.recvfrom()):
+       #sys.stderr.write(str(seqnum) + " ")
+       #if(s.recvfrom(buf)):
+      
+       if(s.sendto(bytes(ack, 'utf-8'), addr)):
+         sys.stderr.write( ack  + "  " + str(seqnum) + " \n ")
+           #sys.stderr.write("Packet received.\n")
+    #s.close
+      
+       
+       
+       #if(s.sendto(bytes(ack, 'utf-8'), addr)): # this sends the acknowledgement
+    #print("Server Ack: " + ack)
+       
+       
+       #send ack to sender
+       #if(s.sendto(bytes(ack, 'utf-8'), addr)): # this sends the acknowledgement
+        # print("Server Ack: " + ack)
+      
+         
+       #sys.stderr.write("Data: " + str(data.decode()) + "Sequence Number: " + str(seqnum))
         #f.write(data)
         #s.sendto(data,addr)
     
 except timeout:
     s.close()
     sys.stderr.write("File Received, exiting.\n")
+    
+  
+  
+#------------------------------main program end----------------------------------------
