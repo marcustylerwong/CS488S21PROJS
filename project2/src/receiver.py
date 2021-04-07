11cfde2724d07b7528045ba9bd161be0a9debb1e
Marcus Wong <marcustylerwong0@gmail.com>
2021-04-06 21:19:56 -0400

    update sender & reciever

--- project2/src/receiver.py
+++ project2/src/receiver.py
@@ -1,4 +1,4 @@
-# Marcus Wong, Laurie Delinois 
+# Marcus Wong, Laurie Delinois
 # ----- receiver.py -----
 
 #!/usr/bin/env python
@@ -6,10 +6,21 @@
 from socket import *
 import sys
 import select
+import time
+import json
+import base64
+import struct
 
-#host = sys.argv[1]
-host = "127.0.0.1"
-port = int(sys.argv[1])
+
+
+
+
+#------------------------------------running main program------------------------------
+#sys.argv for testing
+host = sys.argv[1]
+#host = "127.0.0.1"
+port = int(sys.argv[2])
+#port = int(sys.argv[1])
 
 #start socket and bind to sender
 s = socket(AF_INET,SOCK_DGRAM)
@@ -17,22 +28,81 @@ s.bind((host,port))
 
 addr = (host,port)
 buf=1024
-
+counter = 0
+seqnum = 0
+ack = "Acknowledgement"
 
 #f = open("received.txt",'wb')
 
 data,addr = s.recvfrom(buf)
+#here, we need to set data equal to just the packet end of the dictionary, which requires decoding the packet to reprint it here
+#data = data.decode()
+#packet = data.decode().get('packets')
+#decoded_dict = str(data).decode('utf-8')
+
+#decoded = base64.b64decode(data)
+
+#decoded_ascii = decoded.decode('ascii')
+
+#decoded_ascii = decoded_ascii.replace("'", "\"")
+
+#decoded_dict = json.loads(decoded_ascii) #this converts the input data from dict to encoded bytes to dict
+"""
+ip_header = data.decode()[0:20]
+udp_header = data.decode()[20:28]
+packet = data.decode()[30:]
+#header = struct.unpack('!BBHHHBBH4s4s', udp_header)
+"""
 
 
 try:
     while(data):
+       #so this end is receiving the packet as a dictionary, but is writing the dictionary itself to the file rather than the packets, which is incorrect
+       #decoded = base64.b64decode(data)
+       #sys.stdout.write(data.decode())
        sys.stdout.write(data.decode())
-       #f.write(data)
-       s.settimeout(2)
+
+       #decoded_dict = (data.decode())
+       #decoded_dict = data.decode('seqnum')
+       #sys.stderr.write(decoded_dict + " \n")
+       #data,addr = s.recvfrom(buf)
+       #counter = counter + 1
+       seqnum = seqnum + 1
+
+       s.settimeout(3)
+
        data,addr = s.recvfrom(buf)
+       #sys.stderr.write(str(data))
+       #if(s.recvfrom()):
+       #sys.stderr.write(str(seqnum) + " ")
+       #if(s.recvfrom(buf)):
+
+       if(s.sendto(bytes(ack, 'utf-8'), addr)):
+         sys.stderr.write( ack  + "  " + str(seqnum) + " \n ")
+         #sys.stderr.write("ack is working \n")
+           #sys.stderr.write("Packet received.\n")
+    #s.close
+
+
+
+       #if(s.sendto(bytes(ack, 'utf-8'), addr)): # this sends the acknowledgement
+    #print("Server Ack: " + ack)
+
+
+       #send ack to sender
+       #if(s.sendto(bytes(ack, 'utf-8'), addr)): # this sends the acknowledgement
+        # print("Server Ack: " + ack)
+
+
+       #sys.stderr.write("Data: " + str(data.decode()) + "Sequence Number: " + str(seqnum))
         #f.write(data)
         #s.sendto(data,addr)
-    
+
 except timeout:
     s.close()
     sys.stderr.write("File Received, exiting.\n")
+    #sys.stderr.write(decoded_dict)
+
+
+
+#------------------------------main program end----------------------------------------
--- project2/src/sender.py
+++ project2/src/sender.py
@@ -6,42 +6,172 @@
 from socket import *
 import sys
 import time
+import json
+import base64
 
 
-  
+
 host =sys.argv[1]
-
 port = int(sys.argv[2])
 
 s = socket(AF_INET,SOCK_DGRAM)
+#s.setblocking(0)
 buf =1024
 addr = (host,port)
+#counter = 0
 
 #f=open("a.txt","rb")
 #f = open("a.txt", "rb")
 
-data = sys.stdin.read(buf).encode()
+
+#possibly needs to take this in as a string for packet and not be encoded already?
+#no, simply send buffer size to packet instead of having it read in directly
+
+#data = sys.stdin.read(buf).encode()
+
+#write data in from command line
+
+data = sys.stdin.read().encode()
+
+"""
+start_time = time.time()
+total_kb = 0
+"""
+#-----------------------------make packets-----------------------------------
+def create_packet(data):
+
+
+  #array = bytearray(data.encode()) #bug here with setting up packet split
+  #packet = [array[i:i + 1024] for i in range(0, len(array), 1024)]
+ # header = datagram(data)
+
+  #make the data into a list of bytes to iterate through
+  seqnum = 0
+
+  new_seqnum = str(seqnum)
+
+  b_arr = bytearray(data)
+  b_arr.append(seqnum)
+
+
+  #array = bytearray(data)
+  #pass data into packet object
+  packet = [b_arr[i:i + buf] for i in range(0, len(b_arr), buf)]
+  #https://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks/312464#312464
+  #packets = packet + header
+
+  return packet
+
+#packets = create_packet(data)
+#arr_index = 0
+
+"""
+#--------------------------------make header-------------------------
+
+def datagram(packets):
+
+  seqnum = 0
+  #header = {'seqnum' : seqnum}
+  pack = {'packets': packets, 'seqnum': seqnum}
+
+  rudp = header.get('seqnum')
+  return pack
+
+#-------------------------------header done------------------------------
+"""
+
+#data = sys.stdin.read(buf).encode()
 
 start_time = time.time()
 total_kb = 0
+seqnum = 0
+packets = create_packet(data)
+
+arr_index = 0
+#seqnum = 0
+ACK = " "
+s.settimeout(0.5)
+#header = datagram(data)
+#pack = {'packets' : packets[arr_index], 'seqnum' : seqnum}
+#while(data):
+
+while (arr_index < len(packets)):
+    """ idea: have packets and sequence numbers in the same dictionary """
 
 
-while (data):  
-    #sys.stdin.read(buf)
-    if(s.sendto(data,addr)):
-      #s.sendto(bytearray(buf),addr)
-       
-        print("sending ...")
-        data = sys.stdin.read(buf).encode()
-        
+
+    #packets are written into dictionary with sequence number attached to array index
+    #pack = {"packets" : packets[arr_index], "seqnum" : seqnum}
+   # pack = {str(seqnum) : packets}
+    #datagram = {'seqnum' : seqnum}
+    #encoded_dict = json.dumps(pack).encode('utf-8')
+    #encoded_dict = str(pack.get('packets')).encode('utf-8')
+
+    #message = str(pack)
+    #encoded_dict = str(pack).encode('ascii')
+    #encoded_dict = message.encode('ascii')
+    #pack_encoded_byte = base64.b64encode(encoded_dict)
+
+    #if(s.sendto(packets[arr_index],addr)):
+
+    if(s.sendto(packets[arr_index], addr)):
+    #if(s.sendto(encoded_dict, addr)):
+    #if(s.sendto(pack_encoded_byte, addr)):
+    #if(s.sendto(pack.get('packets'), addr)):
+    #if(s.sendto(pack_encoded_byte, addr)):
+
+
+
+
+
+        #print("sending ...")
+        arr_index = arr_index + 1
+        #seqnum = arr_index
+        seqnum += 1
+        #keep track of time and total kb
         current_time = time.time()
         elapsed_time = current_time - start_time
         total_kb = total_kb + 1
-      
+
+        #print("Sequence Number: " + str(pack.get('seqnum')))
+        print("Sequence Number: " + str(seqnum))
+
+       #--------------------------------------------------------------don't touch for now-----------------------------------------------------------
+
+        acknowledged = False
+        #must use recvfrom here to check that acknowledgements are received
+        #waiting to receive ack from receiver
+        #while not acknowledged: #while acknowledged is true
+        while not acknowledged:
+          try:
+            ACK, addr = s.recvfrom(1024)
+
+            acknowledged = True
+
+            #print("Ack Received")
+          except timeout:
+            s.sendto(packets[arr_index], addr)
+            #s.sendto(pack.get('packets'), addr)
+            #s.settimeout(2)
+        print (ACK)
+
+        #----------------------------------------------------------don't touch for now------------------------------------------------------------------
+
+            #print("Ack")
+
+        #recognize ack from receiver
+        """
+        current_time = time.time()
+        elapsed_time = current_time - start_time
+        total_kb = total_kb + 1
+      """
 s.close()
+
+sys.stderr.write("All acks received.\n")
 
 elapsed_time = float("%0.3f" % (elapsed_time))
 bytes = total_kb * 1024
 kb_sec = total_kb/elapsed_time
 kb_sec = float("%0.3f" % (kb_sec))
 print("Sent " + str(bytes) + " bytes in " + str(elapsed_time) + " seconds: " + str(kb_sec) + " kB/s")
+print("Packets sent: " + str(arr_index))
